6ab3d54046d14e8dc04302a54bf4739465cb3794
Shreya <shreya25.yada@gmail.com>
2023-02-13 22:38:32 +0530

    serving html file

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const port = process.env.PORT || 5010;
 
 app.get("/", function(req, res){
-	res.send("I am Shreya");
+	//res.send("I am Shreya");
+	res.sendFile(__dirname+"/index.html")
 });
 
 app.listen(port, function(){
