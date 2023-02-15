cce9109d14e00b99aca2b1ca2f9763393ec93f8c
Shreya <shreya25.yada@gmail.com>
2023-02-15 23:39:51 +0530

    creating a entry in mongodb

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
+const userLib = require("./backend/lib/userLib");
 const mongoose=require("mongoose");
 
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5010;
+const port = process.env.PORT || 5050;
 
 app.get("/", function(req, res){
 	//res.send("I am Shreya");
@@ -24,6 +25,16 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
 	}
 	else{
 		console.log("DB connected");
+		userLib.createFirstUser(function(err,res)
+		{
+			if(err)
+			{
+				console.error(err);
+			}
+			else{
+				console.log(res);
+			}
+		});
         app.listen(port, function(){
 			console.log("Server running on http://localhost:"+port);
 			console.log(`Server running on http://localhost:${port}`);
