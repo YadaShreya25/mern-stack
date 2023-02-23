4465d43aa1814a387b6c64ea2114e63a96971991
Shreya <shreya25.yada@gmail.com>
2023-02-23 20:29:15 +0530

    todo app with curd operations

--- server.js
+++ server.js
@@ -1,16 +1,120 @@
 require('dotenv').config();
-const userLib = require("./backend/lib/userLib");
-const mongoose=require("mongoose");
 
+const userLib = require("./backend/lib/userLib");
+const todoLib = require("./backend/lib/todoLib");
+const mongoose=require("mongoose");
+const port = process.env.PORT || 5050;
 
 const express = require('express');
 const app = express();
-app.use(express.static("public"));
-const port = process.env.PORT || 5050;
 
+//whatwver front end sends to us it will put it express.json!
+app.use(express.json());
+app.use(express.static("public"));
+
+//function(request,response)
 app.get("/card", function(req, res){
 	//res.send("I am Shreya");
 	res.sendFile(__dirname+"/card.html")
+});
+
+app.get("/todo", function(req, res){
+	//res.send("I am Shreya");
+	res.sendFile(__dirname+"/public/todo.html")
+});
+
+/*app.get("/api/todos", function(req, res){
+	todoLib.getAllTodos(function(err,todos){
+       if(err){
+		   res.json({status: "error",message:err,data:null});
+	   }
+	   else{
+		res.json({status :"success",data:todos});
+	   }
+	});
+});
+
+app.post("/api/todos",function(req,res){
+    const todo = req.body;
+	todoLib.createTodo(todo,function(err,dbtodo){
+		if(err){
+			res.json({status: "error", message: err, data: null});
+		}
+		else{
+		 res.json({status :"success",data: dbtodo});
+		}
+	});
+});
+app.put("/api/todos/:todoId",function(req,res){
+	const todo = req.body;
+	const todoid = req.params.todoId;
+	todoLib.updateTodoById(todoid,todo,function(err,result){
+		if(err){
+			res.json({status: "error", message: err, data: null});
+		}
+		else{
+		 res.json({status :"success",data: result});
+		}
+	});
+});
+
+app.delete("/api/todos/:todoId",function(req,res){
+	const todoid = req.params.todoId;
+	todoLib.deleteTodoById(todoid,function(err,result){
+		if(err){
+			res.json({status: "error", message: err, data: null});
+		}
+		else{
+		 res.json({status :"success",data: result});
+		}
+	});
+});*/
+app.get("/api/todos",function(req,res){
+	todoLib.getAllTodos(function(err,todos){
+		if(err){
+			res.json({status : "error", message : err, data : null});
+		}
+		else{
+			res.json({status : "success", data : todos});
+		}	
+	});
+});
+
+app.post("/api/todos", function(req,res){
+	const todo = req.body;
+	todoLib.createTodo(todo, function(err,dbtodo){
+		if(err){
+			res.json({status : "error", message : err, data : null});
+		}
+		else{
+			res.json({status : "success", data : dbtodo});
+		}
+	});
+});
+
+app.put("/api/todos/:todoid",function(req,res){
+	const todo = req.body;
+	const todoid = req.params.todoid;
+	todoLib.updateTodoById(todoid,todo,function(err,dbtodo){
+		if(err){
+			res.json({status : "error", message : err, data : null});
+		}
+		else{
+			res.json({status : "success", data : dbtodo});
+		}
+	});
+});
+
+app.delete(("/api/todos/:todoid"),function(req,res){
+	const todoid = req.params.todoid;
+	todoLib.deleteTodoById(todoid, function(err,dbtodo){
+		if(err){
+			res.json({status: "error", message: err, data: null});
+		}
+		else{
+			res.json({status: "success", data: dbtodo});
+		}
+	});
 });
 
 
@@ -66,7 +170,7 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
 				console.log(result);
 			}
 		});*/
-		userLib.deleteUser("YadaShreya",function(err,result)
+		/*userLib.deleteUser("YadaShreya",function(err,result)
 		{
 			if(err)
 			{
@@ -75,7 +179,7 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
 			else{
 				console.log(result);
 			}
-		});
+		});*/
 		/*userLib.getUserByFilter({userName : "YadaShreya"},function(err,result){
 			if(err){
 				console.error(err);
